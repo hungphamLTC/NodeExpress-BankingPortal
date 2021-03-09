61334d3c38780d5e3dae761cba328eb5cd4c0f69
Hung Pham <hung.pham4490@gmail.com>
2021-03-09 19:14:50 +0200

    update module 2

--- src/app.js
+++ src/app.js
@@ -10,10 +10,13 @@ app.use(express.static(path.join(__dirname, '/public')));
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'));
+const users = JSON.parse(userData);
+
 app.get('/', (req, res)=>res.render('index', {title: 'Account Summary', accounts: accounts}));
 app.get('/savings', (req, res)=>res.render('account', {account: accounts.savings}));
 app.get('/checking', (req, res)=> res.render('account', {account: accounts.checking}));
-app.get('/credit', (req, res)=> res.render('account', {account: account.credit}));
+app.get('/credit', (req, res)=> res.render('account', {account: accounts.credit}));
 app.get('/profile', (req, res)=> res.render('profile', {user: users[0]}));
 app.listen(3000, ()=> console.log("Listening on port 3000"));
 
