17411330ede5fc1ca0a32cfe30767af347a3a395
Hung Pham <hung.pham4490@gmail.com>
2021-03-09 19:11:58 +0200

    complete module 2

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res)=>res.render('index', {title: 'Index'}));
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
 
+app.get('/', (req, res)=>res.render('index', {title: 'Account Summary', accounts: accounts}));
+app.get('/savings', (req, res)=>res.render('account', {account: accounts.savings}));
+app.get('/checking', (req, res)=> res.render('account', {account: accounts.checking}));
+app.get('/credit', (req, res)=> res.render('account', {account: account.credit}));
+app.get('/profile', (req, res)=> res.render('profile', {user: users[0]}));
 app.listen(3000, ()=> console.log("Listening on port 3000"));
 
-//path, fs, express, app
-//set view: view engine ejs
